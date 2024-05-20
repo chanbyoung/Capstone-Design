cb4ddf9241e36f790b517d26610ef9260da42ca9
pcb <pcb7893@naver.com>
2024-05-20 13:11:36 +0900

    HOTFIX: enum 타입 파싱 안되는 오류 수정

--- src/main/java/durikkiri/project/entity/FieldCategory.java
+++ src/main/java/durikkiri/project/entity/FieldCategory.java
@@ -1,5 +1,7 @@
 package durikkiri.project.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,5 +11,20 @@ public enum FieldCategory {
     FRONTEND("프론트엔드"), BACKEND("백엔드"), GENERAL("일반 참여자");
 
     private final String value;
+
+    @JsonCreator
+    public static FieldCategory fromValue(String value) {
+        for (FieldCategory category : FieldCategory.values()) {
+            if (category.value.equals(value) || category.name().equalsIgnoreCase(value)) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("Invalid value for FieldCategory: " + value);
     }
+
+    @JsonValue
+    public String toValue() {
+        return this.value;
+    }
+}
 
