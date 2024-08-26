c71fee91417a699c6cb7f668f8b7abf8c30e2b4b
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-08-27 02:11:12 +0900

    Chore: build.gradle, yml 수정

--- src/main/java/durikkiri/project/ProjectApplication.java
+++ src/main/java/durikkiri/project/ProjectApplication.java
@@ -1,14 +1,10 @@
 package durikkiri.project;
 
-import durikkiri.project.security.SpringSecurityAuditorAware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
 public class ProjectApplication {
 
 	public static void main(String[] args) {
